faa477ee025a7a282f79c526938f7da24cfcc92b
Howard Feger <hfeger4@gmail.com>
2018-10-10 09:36:58 -0700

    add max profit

--- ruby.rb
+++ ruby.rb
@@ -1,9 +1,104 @@
-def quicksort(arr)
-  return arr if arr.length <= 1
-  pivot = arr.first
-  left = quicksort(arr[1..-1].select{|el| el <= pivot})
-  right = quicksort(arr[1..-1].select{|el| el > pivot})
-  return left + [pivot] + right
+# def quicksort(arr)
+#   return arr if arr.length <= 1
+#   pivot = arr.first
+#   left = quicksort(arr[1..-1].select{|el| el <= pivot})
+#   right = quicksort(arr[1..-1].select{|el| el > pivot})
+#   return left + [pivot] + right
+# end
+#
+# quicksort([1,6,2,3,5,4])
+#
+# # //remove duplicates from arr in place and return arr
+#
+# function removeDups(nums){
+#   let i = 0;
+#   for (let j = 1; j < nums.length; j++){
+#     if (nums[i] !== nums[j]){
+#       i++;
+#       nums[i] = nums[j];
+#     }
+#   }
+#   return nums.slice(0,i+1);
+# }
+#
+# console.log(removeDups([1,1,1,2,2,3,3,3,3,3,3]));
+#
+# # //mergesort
+#
+# function mergeSort(arr){
+#   if (arr.length === 1){
+#     return arr;
+#   }
+#
+#   let mid = Math.floor(arr.length/2);
+#   let left = arr.slice(0,mid);
+#   let right = arr.slice(mid);
+#
+#   let sortedLeft = mergeSort(left);
+#   let sortedRight = mergeSort(right);
+#   return merge(sortedLeft, sortedRight);
+# }
+#
+# function merge(left,right){
+#   let arr = [];
+#   while (left.length > 0 && right.length > 0){
+#     if (left[0] < right[0]){
+#       arr.push(left.shift());
+#     }else{
+#       arr.push(right.shift());
+#     }
+#   }
+#   if (left.length > 0){
+#     arr.concat(left);
+#   }
+#   if (right.length > 0){
+#     arr.concat(right);
+#   }
+#   return arr.concat(left).concat(right);
+# }
+#
+# console.log(mergeSort([1,5,2,3,4]));
+#
+# # //bsearch for position
+# function b_search(arr,left,right,target){
+#
+#   while(left <= right){
+#     let mid = (left + right) >> 1;
+#     if(target >= arr[mid]){
+#       left = mid + 1;
+#     }else{
+#       right = mid - 1;
+#     }
+#   }
+#   return right;
+# }
+#
+# # //add biggest difference
+#
+# function biggest_difference(arr){
+#   let biggest_diff = -1;
+#   let min = arr[0];
+#   for(let i = 1; i < arr.length; i++){
+#     let temp_big = arr[i] - min;
+#     biggest_diff = biggest_diff > temp_big ? biggest_diff : temp_big;
+#     min = min > arr[i] ? arr[i] : min;
+#   }
+#   return biggest_diff;
+# }
+
+#Max profit with K interactions
+
+def max_profit_k(arr)
+  min = arr[0]
+  largest_diff = 0
+  i = 1
+  while i < arr.length
+    current_diff = arr[i] - min
+    largest_diff = [largest_diff,current_diff].max
+    min = [arr[i],arr[0]].min
+    i += 1
+  end
+  largest_diff
 end
 
-quicksort([1,6,2,3,5,4])
+puts max_profit_k([5,11,3,50,60,90])
